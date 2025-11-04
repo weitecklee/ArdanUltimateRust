5b49082f846e6e4cea9595d9d2404ef0be615f9a
arkteck <arkteck@gmail.com>
2025-11-04 13:21:22 -0500

    scoped threads

---
+++ session2/scoped_threads/src/main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+
+const N_THREADS: usize = 8;
+
+fn main() {
+    let to_add: Vec<u32> = (0..5000).collect();
+    let chunks = to_add.chunks(N_THREADS);
+    let sum = thread::scope(|s| {
+        let mut thread_handles = Vec::new();
+        for chunk in chunks {
+            let thread_handle = s.spawn(move || chunk.iter().sum::<u32>());
+            thread_handles.push(thread_handle);
+        }
+        thread_handles
+            .into_iter()
+            .map(|h| h.join().unwrap())
+            .sum::<u32>()
+    });
+    println!("{sum}");
+}
