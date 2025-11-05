7ea0e007837f489113bdbeb44819fc2e5cf81e0d
arkteck <arkteck@gmail.com>
2025-11-05 14:05:29 -0500

    await, spawn, and yield with tokio

--- session3/hello_tokio/src/main.rs
+++ session3/hello_tokio/src/main.rs
@@ -1,14 +1,40 @@
 // use tokio::runtime;
 
-async fn hello() {
+async fn hello() -> u32 {
     println!("Hello Tokio!");
+    1
+}
+
+async fn hello2() -> u32 {
+    println!("Hello Tokio2!");
+    2
+}
+
+async fn ticker() {
+    for i in 0..10 {
+        println!("tick {i}");
+        tokio::task::yield_now().await;
+    }
 }
 
 // #[tokio::main(flavor = "current_thread")]
 // #[tokio::main(flavor = "multi_thread")]
 #[tokio::main]
 async fn main() {
-    hello().await;
+    // hello().await;
+    // let result = tokio::join!(hello(), hello2());
+    // println!("{result:?}");
+
+    // tokio::spawn(ticker());
+    // hello().await;
+
+    let _ = tokio::join!(
+        tokio::spawn(hello()),
+        tokio::spawn(ticker()),
+        tokio::spawn(hello2()),
+        tokio::spawn(ticker()),
+    );
+    println!("Finished!");
 }
 
 // fn main() {
