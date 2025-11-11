75a5c1701c5dee8857d797e41bed71306976b4ad
arkteck <arkteck@gmail.com>
2025-11-11 10:34:11 -0500

    low level memory management

--- session4/memory/src/main.rs
+++ session4/memory/src/main.rs
@@ -1,14 +1,38 @@
-#![allow(dead_code, clippy::useless_vec)]
+fn allocate_memory_with_libc() {
+    unsafe {
+        // Allocate memory with libc (one 32-bit integer)
+        let my_num: *mut i32 = libc::malloc(std::mem::size_of::<i32>() as libc::size_t) as *mut i32;
+        if my_num.is_null() {
+            panic!("failed to allocate memory");
+        }
 
-/// # Safety
-///
-/// This function is unsafe because unsafe functions need documentation.
-unsafe fn my_fn() {}
+        // Set the allocated variable - dereference the pointer and set to 42
+        *my_num = 42;
+        assert_eq!(42, *my_num);
+
+        // Free the memory with libc - this is NOT automatic
+        libc::free(my_num as *mut libc::c_void);
+    }
+}
+
+fn allocate_memory_with_rust() {
+    use std::alloc::{Layout, alloc, dealloc};
+
+    unsafe {
+        // Allocate memory with Rust. It's safer to force alignment.
+        let layout = Layout::new::<u16>();
+        let ptr = alloc(layout);
+
+        // Set the allocated variable - dereference the pointer and set to 42
+        *ptr = 42;
+        assert_eq!(42, *ptr);
+
+        // Free the memory - this is not automatic
+        dealloc(ptr, layout);
+    }
+}
 
 fn main() {
-    let my_vec = vec![1, 2, 3, 4, 5];
-    unsafe {
-        let val = my_vec.get_unchecked(20);
-        println!("{val}");
-    }
+    allocate_memory_with_libc();
+    allocate_memory_with_rust()
 }
