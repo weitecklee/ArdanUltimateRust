79944b025bf02ad0f559d2a2e56bc4d07f562577
arkteck <arkteck@gmail.com>
2025-11-13 14:05:48 -0500

    web service v1

--- session5/server/src/collector.rs
+++ session5/server/src/collector.rs
@@ -1,11 +1,12 @@
-use shared_data::{DATA_COLLECTOR_ADDRESS, decode_v1};
+use shared_data::{CollectorCommandV1, DATA_COLLECTOR_ADDRESS, decode_v1};
+use sqlx::{Pool, Sqlite};
 use std::net::SocketAddr;
 use tokio::{
     io::AsyncReadExt,
     net::{TcpListener, TcpStream},
 };
 
-pub async fn data_collector() -> anyhow::Result<()> {
+pub async fn data_collector(cnn: Pool<Sqlite>) -> anyhow::Result<()> {
     // Listen for TCP connections on the data collector address
     let listener = TcpListener::bind(DATA_COLLECTOR_ADDRESS).await?;
 
@@ -13,13 +14,13 @@ pub async fn data_collector() -> anyhow::Result<()> {
     loop {
         // Wait for a new connection
         let (socket, address) = listener.accept().await?;
-        tokio::spawn(new_connection(socket, address));
+        tokio::spawn(new_connection(socket, address, cnn.clone()));
     }
 }
 
-async fn new_connection(mut socket: TcpStream, address: SocketAddr) {
+async fn new_connection(mut socket: TcpStream, address: SocketAddr, cnn: Pool<Sqlite>) {
     println!("New connection from {address:?}");
-    let mut buf = vec![0u8; 1024];
+    let mut buf = vec![0_u8; 1024];
     loop {
         let n = socket
             .read(&mut buf)
@@ -34,5 +35,33 @@ async fn new_connection(mut socket: TcpStream, address: SocketAddr) {
         println!("Received {n} bytes");
         let received_data = decode_v1(&buf[0..n]);
         println!("Received data: {received_data:?}");
+
+        match received_data {
+            (
+                timestamp,
+                CollectorCommandV1::SubmitData {
+                    collector_id,
+                    total_memory,
+                    used_memory,
+                    average_cpu_usage,
+                },
+            ) => {
+                let collector_id = uuid::Uuid::from_u128(collector_id);
+                let collector_id = collector_id.to_string();
+
+                let result = sqlx::query("INSERT INTO timeseries (collector_id, received, total_memory, used_memory, average_cpu) VALUES ($1, $2, $3, $4, $5)")
+                    .bind(collector_id)
+                    .bind(timestamp)
+                    .bind(total_memory as i64)
+                    .bind(used_memory as i64)
+                    .bind(average_cpu_usage)
+                    .execute(&cnn)
+                    .await;
+
+                if result.is_err() {
+                    println!("Error inserting data into the database: {result:?}");
+                }
+            }
+        }
     }
 }
--- session5/server/src/main.rs
+++ session5/server/src/main.rs
@@ -1,8 +1,30 @@
+use axum::{Extension, Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+mod api;
 mod collector;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let handle = tokio::spawn(collector::data_collector());
+    // Read the .env file and obtain the database URL
+    dotenv::dotenv()?;
+    let db_url = std::env::var("DATABASE_URL")?;
+
+    // Get a database connection pool
+    let pool = sqlx::SqlitePool::connect(&db_url).await?;
+
+    let handle = tokio::spawn(collector::data_collector(pool.clone()));
+
+    // Start the web server
+    let app = Router::new()
+        .route("/api/all", get(api::show_all))
+        .route("/api/collectors", get(api::show_collectors))
+        .route("/api/collector/{uuid}", get(api::collector_data))
+        .layer(Extension(pool));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(&addr).await?;
+    axum::serve(listener, app.into_make_service()).await?;
 
     // Wait for the data collector to finish
     handle.await??; // Two question marks - we're unwrapping the task result, and the result from running the collector.
